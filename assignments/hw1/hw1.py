564c5c1558d66f11a57f10085df4eac1db71b4f0
arnoldaguila04 <arnoldaguila04@gmail.com>
2022-01-19 09:42:58 -0500

    HW1 Arnold Aguila

--- assignments/hw1/hw1.py
+++ assignments/hw1/hw1.py
@@ -1,34 +1,60 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Arnold Aguila
+hw1.py
 
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+Problem: Calculating the volume of a rectangle, Calculating shooting percentage, Calculating coffee per pound, and
+changing KM to Miles.
 
 Certification of Authenticity:
 <include one of the following>
 I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
+
 """
 
 
 def calc_rec_area():
-    pass
-
+    print("This function calculates the area of a rectangle.")
+    length = eval(input("Enter the length: "))
+    width = eval(input("Enter the width: "))
+    area = length * width
+    print("Area =", area)
+calc_rec_area()
 
 def calc_volume():
-    pass
+    print("This function calculates the volume of a rectangle.")
+    length = eval(input("Enter the length: "))
+    width = eval(input("Enter the width: "))
+    height = eval(input("Enter the height: "))
+    volume = length * width * height
+    print("Volume =", volume)
+calc_volume()
 
 
 def shooting_percentage():
-    pass
+    print("This function calculates your shooting percentage.")
+    shots = eval(input("How many shots fired: "))
+    hits = eval(input("How many hits on target: "))
+    accuracy = (hits / shots ) * 100
+    print("Shooting Percentage =", accuracy, "%")
+shooting_percentage()
 
 
 def coffee():
-    pass
+    print("This function calculates coffee cost per lbs.")
+    user_input = eval(input("How many pounds of coffee purchased: "))
+    x = 10.50 * user_input
+    y = 0.86 * user_input
+    ans = x + y + 1.50
+    print("Your total is:", ans)
+coffee()
 
 
 def kilometers_to_miles():
-    pass
+    print("This function changes km to miles")
+    ans = eval(input("Enter Km: ")) * 0.62137
+    round_ans = round(ans, 2)
+    print("That's", round_ans, "miles!")
+kilometers_to_miles()
 
 
 if __name__ == '__main__':
