bb6e837054349eb94ecd50d2130c1946bced3018
arnoldaguila04 <arnoldaguila04@gmail.com>
2022-01-28 21:20:10 -0500

    HW2 Completed Arnold Aguila

--- assignments/hw2/hw2.py
+++ assignments/hw2/hw2.py
@@ -1,36 +1,59 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: Arnold Aguila
+hw2.py
 
 Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
 
 Certification of Authenticity:
 <include one of the following>
 I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
+
 """
 import math
 
 
+
+
 def sum_of_threes():
-    pass
+    print("This function add the sum of three to the upper limit.")
+    x = eval(input("Enter the upper limit: "))
+    y = 0
+    for i in range(3, x+1, 3):
+        y = y + i
+    print("The sum of threes is: ", y)
 
 
 def multiplication_table():
-    pass
-
+    for i in range(1, 10+1):
+        print()
+        for j in range(1, 10+1):
+            print(i * j, end="   ")
 
 def triangle_area():
-    pass
+    a = eval(input("Enter side 1: "))
+    b = eval(input("Enter side 2: "))
+    c = eval(input("Enter side 3: "))
+    s = (a + b + c) / 2
+    area = math.sqrt(s * (s - a) * (s - b) * (s - c))
+    print("area is: ", area)
 
 
 def sum_squares():
-    pass
+    up = eval(input("Enter the starting range: "))
+    down = eval(input("Enter the ending range: "))
+    y = 0
+    for i in range(up, down+1):
+        y = (i * i) + y
+    print(y)
 
 
 def power():
-    pass
-
+    base = eval(input("Enter base: "))
+    exponent = eval(input("Enter exponent: "))
+    ans = 1
+    for i in range(1, exponent + 1):
+        ans = ans * base
+    print(base, "^", exponent, "=", ans)
 
 if __name__ == '__main__':
     pass
