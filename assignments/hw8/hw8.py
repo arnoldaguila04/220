b18021af5b83a86d20e0715783d8b505c5b39e3a
arnoldaguila04 <arnoldaguila04@gmail.com>
2022-03-18 17:04:21 -0400

    HW6 Completed Arnold Aguila

--- assignments/hw8/hw8.py
+++ assignments/hw8/hw8.py
@@ -1,30 +1,38 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: Arnold
+HW8.py
 
 Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
 
 Certification of Authenticity:
 <include one of the following>
 I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
 """
+import math
+from graphics import *
 
 
 def add_ten(nums):
-    pass
+    for i in range(len(nums)):
+        nums[i] += 10
 
 
 def square_each(nums):
-    pass
+    for i in range(len(nums)):
+        nums[i] **= 2
 
 
 def sum_list(nums):
-    pass
+    total = 0
+    for i in range(len(nums)):
+        total += nums[i]
+    return total
 
 
 def to_numbers(nums):
-    pass
+    for i in range(len(nums)):
+        nums[i] = float(nums[i])
+
 
 
 def sum_of_squares(nums):
@@ -32,11 +40,15 @@ def sum_of_squares(nums):
 
 
 def starter(weight, wins):
-    pass
+    if (weight >= 150 and weight < 160) and (wins >= 5):
+        return True
+    if (weight > 199) or (wins > 20):
+        return True
+    return False
 
 
 def leap_year(year):
-    pass
+    return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
 
 
 def circle_overlap():
@@ -55,12 +67,37 @@ def circle_overlap():
     circle_one.setFill("light blue")
     circle_one.draw(win)
 
+    center2 = win.getMouse()
+    circumference_point2 = win.getMouse()
+    radius2 = math.sqrt(
+        (center2.getX() - circumference_point2.getX()) ** 2 + (center2.getY() - circumference_point2.getY()) ** 2)
+    circle_two = Circle(center2, radius2)
+    circle_two.setFill("light blue")
+    circle_two.draw(win)
+
+    if_message = Text(Point(5, 2), "The circle overlaps")
+    else_message = Text(Point(5, 2), "The circle does not overlap")
+    close_message = Text(Point(5,1), "Click again to close")
+
+    comparing_distance = math.sqrt((center.getX() - center2.getX()) ** 2 + (center.getY() - center2.getY() ** 2))
+    sum_of_two_radius = radius + radius2
+
+    if comparing_distance < sum_of_two_radius:
+        if_message.draw(win)
+    else:
+        else_message.draw(win)
+
+    close_message.draw(win)
     win.getMouse()
 
 
 def did_overlap(circle_one, circle_two):
-    pass
-
+    comparing_distance = math.sqrt((circle_one.getX() - circle_two.getX()) ** 2 + (circle_one.getY() - circle_two.getY() ** 2))
+    sum_of_two_radius = circle_one.getRadius() + circle_two.getRadius()
+    if comparing_distance < sum_of_two_radius:
+        return True
+    else:
+        return False
 
 if __name__ == '__main__':
     pass
